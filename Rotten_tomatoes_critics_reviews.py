844ab556813ef085a28a0dc5445823b6917fa838
Alicja <57335001+Alicja96@users.noreply.github.com>
2020-06-14 15:45:52 +0200

    Update Rotten_tomatoes_critics_reviews.py

--- Rotten_tomatoes_critics_reviews.py
+++ Rotten_tomatoes_critics_reviews.py
@@ -5,7 +5,6 @@ import re
 
 reviews=[] #for storing reviews
 sentiment=[] #for storing the sentiment of reviews
-date = [] #for storing dates
 
 def getData(season,page):
     res = requests.get(f'https://www.rottentomatoes.com/tv/killing_eve/s0{season}/reviews?type=&sort=&page={page}')
@@ -17,20 +16,16 @@ def getData(season,page):
     sentiments=soup.find_all('div',{'class':'review_icon'})
     find_sentiment= re.findall(r'rotten|fresh',str(sentiments))
     
-    dates = soup.find_all('div',{'class':'critic__review-date subtle small'})
-    d = [i.text for i in dates]
-    
     reviews.extend(review)
     sentiment.extend(find_sentiment)
-    date.extend(d)
-    
-    
+
+        
 for season in range(1,4):
     for page in range(1,6):
         getData(season,page)
 
         
-df = pd.DataFrame({'reviews': reviews,'sentiment': sentiment,'date':date})
+df = pd.DataFrame({'reviews': reviews,'sentiment': sentiment})
 
 df['sentiment'].replace(['fresh', 'rotten'], ['positive', 'negative'], inplace=True)
 
