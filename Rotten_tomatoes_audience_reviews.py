e0619d2b78d93983a31b8a1c6c5a47e36ceb27cc
Alicja <57335001+Alicja96@users.noreply.github.com>
2020-06-24 18:08:41 +0200

    Update Rotten_tomatoes_audience_reviews.py

--- Rotten_tomatoes_audience_reviews.py
+++ Rotten_tomatoes_audience_reviews.py
@@ -1,7 +1,7 @@
 from selenium import webdriver      
 from bs4 import BeautifulSoup
 import pandas as pd
-import time
+
     
 driver = webdriver.Chrome(executable_path="/Users/alicja/Downloads/chromedriver")
 
