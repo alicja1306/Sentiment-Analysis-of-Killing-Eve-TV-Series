ec971fdf3551083650e4785f7f68ff6a81813efd
Alicja <57335001+Alicja96@users.noreply.github.com>
2020-07-09 21:43:06 +0200

    Update RottenTomatoesAudienceReviews.py

--- RottenTomatoesAudienceReviews.py
+++ RottenTomatoesAudienceReviews.py
@@ -1,17 +1,14 @@
 from selenium import webdriver      
 from bs4 import BeautifulSoup
 import pandas as pd
-
-    
+   
 driver = webdriver.Chrome(executable_path="/Users/alicja/Downloads/chromedriver")
 
 url = ['https://www.rottentomatoes.com/tv/killing_eve/s01/reviews?type=user','https://www.rottentomatoes.com/tv/killing_eve/s02/reviews?type=user','https://www.rottentomatoes.com/tv/killing_eve/s03/reviews?type=user']
-
     
 star = 0
-sentiment = [] # for storing scores 
-reviews=[] # for storing reviews
-
+sentiment = [] 
+reviews=[] 
 
 for page in url: 
     driver.get(page)
@@ -20,10 +17,10 @@ for page in url:
 
         stars=soup.find_all('span',{'class':'star-display'})
 
-        for i,j in enumerate(stars):
-            if j.find_all('span',{'class':'star-display__filled'}):
-                star = len(j.find_all('span',{'class':'star-display__filled'}))
-            if j.find('span',{'class':'star-display__half'}):
+        for i,item in enumerate(stars):
+            if item.find_all('span',{'class':'star-display__filled'}):
+                star = len(item.find_all('span',{'class':'star-display__filled'}))
+            if item.find('span',{'class':'star-display__half'}):
                 star = star + 0.5
             sentiment.append(star)
     
@@ -33,23 +30,19 @@ for page in url:
         
         try:
             
-            NextButton = driver.find_element_by_css_selector('#content > div > div > nav:nth-child(4) > button.js-prev-next-paging-next.btn.prev-next-paging__button.prev-next-paging__button-right > span')
-            NextButton.click()
+            nextbutton = driver.find_element_by_css_selector('#content > div > div > nav:nth-child(4) > button.js-prev-next-paging-next.btn.prev-next-paging__button.prev-next-paging__button-right > span')
+            nextbutton.click()
             time.sleep(5)
         
         except Exception as e:
             print(e)
             break
-print("Complete")
         
-
 # checking if reviews and sentiment have the same length
 print(len(reviews)) 
 print(len(sentiment))
 
-
 df = pd.DataFrame({'reviews': reviews ,'sentiment': sentiment})
-
 
 df.to_csv('rotten_tomatoes_audience_reviews.csv',index=False) 
 
