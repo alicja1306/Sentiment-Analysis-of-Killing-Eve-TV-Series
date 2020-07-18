3d9489e416a820cc7e769c1b6d2137bc346a1dab
Alicja <57335001+Alicja96@users.noreply.github.com>
2020-07-18 11:55:58 +0200

    Update WebScrapingIMDBReviews.py

--- WebScrapingIMDBReviews.py
+++ WebScrapingIMDBReviews.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
     
-driver = webdriver.Chrome(executable_path="/Users/alicja/Downloads/chromedriver")
+driver = webdriver.Chrome(executable_path = "/Users/alicja/Downloads/chromedriver")
 driver.get('https://www.imdb.com/title/tt7016936/reviews?ref_=tt_urv')
 
 # Load all the page data, by clicking load more button.
@@ -14,32 +14,29 @@ while True:
         print(e)
         break
 
-soup=BeautifulSoup(driver.page_source, 'lxml') 
+soup = BeautifulSoup(driver.page_source, 'lxml') 
 
-div=soup.find_all('div',{'class':'lister-item'})
+div = soup.find_all('div', {'class':'lister-item'})
 
-sentiments=[]
+sentiments = []
 for i in div:
-    if i.find('span',{'class':'rating-other-user-rating'}):
-        sentiments.append(i.find('span',{'class':'rating-other-user-rating'}).span.text)
+    if i.find('span', {'class':'rating-other-user-rating'}):
+        sentiments.append(i.find('span', {'class':'rating-other-user-rating'}).span.text)
     else:
         sentiments.append(0) #if user rate doesn't exist
 
 reviews=[]
 for i in div:
-    if i.find('div',{'class':"text show-more__control"}):
-        reviews.append(i.find('div',{'class':"text show-more__control"}).text)
+    if i.find('div', {'class':"text show-more__control"}):
+        reviews.append(i.find('div', {'class':"text show-more__control"}).text)
     else:
         reviews.append(0) #if review doesn't exist
         
-# checking if reviews and sentiments have the same length
-print(len(sentiments))
-print(len(reviews))
-       
-df = pd.DataFrame({'reviews': reviews,'sentiment': sentiments})
+df = pd.DataFrame({'reviews': reviews, 'sentiment': sentiments})
 
 df.head()
 
 data = df[df.sentiment != 0] #removing rows without user rate
+data = df[df.reviews != 0] #removing rows without reviews
 
-data.to_csv('imbd_reviews.csv',index=False)
+data.to_csv('imbd_reviews.csv', index = False)
