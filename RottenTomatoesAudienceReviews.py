0540d73ad23c6e65229578de38b6c98c4fccbb4f
Alicja <57335001+Alicja96@users.noreply.github.com>
2020-07-18 11:45:21 +0200

    Update RottenTomatoesAudienceReviews.py

--- RottenTomatoesAudienceReviews.py
+++ RottenTomatoesAudienceReviews.py
@@ -2,46 +2,39 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
    
-driver = webdriver.Chrome(executable_path="/Users/alicja/Downloads/chromedriver")
+driver = webdriver.Chrome(executable_path = "/Users/alicja/Downloads/chromedriver")
 
-url = ['https://www.rottentomatoes.com/tv/killing_eve/s01/reviews?type=user','https://www.rottentomatoes.com/tv/killing_eve/s02/reviews?type=user','https://www.rottentomatoes.com/tv/killing_eve/s03/reviews?type=user']
+url = ['https://www.rottentomatoes.com/tv/killing_eve/s01/reviews?type=user', 'https://www.rottentomatoes.com/tv/killing_eve/s02/reviews?type=user', 'https://www.rottentomatoes.com/tv/killing_eve/s03/reviews?type=user']
     
 star = 0
 sentiment = [] 
-reviews=[] 
+reviews = [] 
 
 for page in url: 
     driver.get(page)
     while True:
-        soup=BeautifulSoup(driver.page_source, 'lxml') 
-
-        stars=soup.find_all('span',{'class':'star-display'})
-
-        for i,item in enumerate(stars):
-            if item.find_all('span',{'class':'star-display__filled'}):
-                star = len(item.find_all('span',{'class':'star-display__filled'}))
-            if item.find('span',{'class':'star-display__half'}):
+        soup = BeautifulSoup(driver.page_source, 'lxml') 
+        stars = soup.find_all('span', {'class':'star-display'})
+      
+        for i, item in enumerate(stars):
+            if item.find_all('span', {'class':'star-display__filled'}):
+                star = len(item.find_all('span', {'class':'star-display__filled'}))
+            if item.find('span', {'class':'star-display__half'}):
                 star = star + 0.5
             sentiment.append(star)
     
-        review=soup.find_all('p',{'class':'audience-reviews__review--mobile js-review-text clamp clamp-4 js-clamp'})
+        review = soup.find_all('p', {'class':'audience-reviews__review--mobile js-review-text clamp clamp-4 js-clamp'})
         for i in review:
             reviews.append(i.text)
         
         try:
-            
             nextbutton = driver.find_element_by_css_selector('#content > div > div > nav:nth-child(4) > button.js-prev-next-paging-next.btn.prev-next-paging__button.prev-next-paging__button-right > span')
             nextbutton.click()
             time.sleep(5)
-        
-        except Exception as e:
+         except Exception as e:
             print(e)
             break
-        
-# checking if reviews and sentiments have the same length
-print(len(reviews)) 
-print(len(sentiment))
 
-df = pd.DataFrame({'reviews': reviews ,'sentiment': sentiment})
+df = pd.DataFrame({'reviews': reviews , 'sentiment': sentiment})
 
-df.to_csv('rotten_tomatoes_audience_reviews.csv',index=False) 
+df.to_csv('rotten_tomatoes_audience_reviews.csv', index=False) 
